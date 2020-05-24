6c7b71134199cf04f07acf95671447a3aa088f45
Kemal TUNÇ <kemaltunc191@gmail.com>
2020-05-24 05:55:26 +0300

    Add api protect

--- routes/AuthRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const authController = require('../controllers/AuthController')
-
-router.post('/signup', authController.signup)
-router.post('/login', authController.login)
-
-
-module.exports = router
--- routes/Routes.js
+++
@@ -1,8 +0,0 @@
-
-const authRote = require('./AuthRoute')
-const postRoutes = require('./PostRoutes')
-
-module.exports = function (app) {
-    app.use('/api/v1/users', authRote)
-    app.use('/api/v1/post', postRoutes)
-}
