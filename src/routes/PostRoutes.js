b51f0b06a22dbeb6cded8e223cac6650ce41c6d8
Kemal TUNÇ <kemaltunc191@gmail.com>
2020-05-26 17:17:12 +0300

    Add post service

--- src/routes/PostRoutes.js
+++ src/routes/PostRoutes.js
@@ -4,7 +4,7 @@ const postController = require('../controllers/PostController')
 const auth = require('../middlewares/Authenticated')
 
 
-//router.use(auth.protect)
+router.use(auth.protect)
 router.post('/create', postController.insert)
 router.get('/getAll',postController.getAll)
 module.exports = router
